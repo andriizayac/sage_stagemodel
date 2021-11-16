7cd8d7182ea11a09236530a5755a6518d6bd41c1
[Andrii Zaiats] <[andriizaiats@u.boisestate.edu]>
2021-11-16 08:35:28 -0700

    integrated Payton's description

--- util.jl
+++ util.jl
@@ -1,6 +1,6 @@
-# ---------------------------------------------------------------
-# create pdf
-using Weave, DSP
-weave(joinpath(pwd(), "model_description.jmd"),
-  out_path = joinpath(pwd(), "model_description_pdf"),
-  doctype = "md2pdf")
+  # ---------------------------------------------------------------
+  # create pdf
+  using Weave, DSP, Plots
+  weave(joinpath(pwd(), "model_description.jmd"),
+    out_path = joinpath(pwd(), "model_description_pdf"),
+    doctype = "md2pdf") 
