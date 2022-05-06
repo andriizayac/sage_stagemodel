e249c1e88c34bd34f0f88d189701e159dafdc871
[Andrii Zaiats] <[andriizaiats@u.boisestate.edu]>
2022-05-06 09:49:03 -0600

    added save/load example

--- util.jl
+++ util.jl
@@ -8,9 +8,19 @@
 # ---------------------------------------------------------------
  # create a gif animation 
  # requires a series of Plots
- anim = @animate for i in 1:100#size(hmat)[3]
-  plot(hmat[:,:,i], st = :surface, zlims = [0, 25],
-    xlabel = "x", ylabel = "y",  zlabel = "Population size, H_t", 
-    title = string("t = ", i, "/", size(hmat)[3]))
+anim = @animate for i in 1:10#size(hmat)[3]
+  plot(img[:,:,i], st = :surface, zlims = [0, 20],
+    xlabel = "x", ylabel = "y",  zlabel = "Population size, H_t")
 end every 1
-gif(anim, "anim0_fps10_0.gif", fps = 12)
+gif(anim, "anim0_out.gif", fps = 2)
+
+# === example of saving the output
+using JLD
+a = rand(10,10)
+b = rand(1000,100, 5)
+JLD.save("mydata.jld", "a", a, "b", b)
+
+ain = JLD.load("mydata.jld", "a")
+bin = JLD.load("mydata.jld", "b")
+
+h = JLD.load("mydata.jld","b") 
